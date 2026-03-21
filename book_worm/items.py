045049c0da9ec5b6254a83d0ffe37998869abec9
sudipsudip001 <sudipshrestha2051219@gmail.com>
2026-03-21 14:26:57 +0545

    v2: absolute error handling, efficient code with multiple improvements

--- book_worm/items.py
+++ book_worm/items.py
@@ -8,5 +8,12 @@ import scrapy
 
 class BookWormItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    url = scrapy.Field()
+    scrape_date = scrapy.Field()
+    description = scrapy.Field()
+    price = scrapy.Field()
+    tax = scrapy.Field()
+    availability = scrapy.Field()
+    upc = scrapy.Field()
+    rating = scrapy.Field()
