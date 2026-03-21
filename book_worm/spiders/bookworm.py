d922ec8bd911c99a86bb15f4b89fa4731b445adc
sudipsudip001 <sudipshrestha2051219@gmail.com>
2026-03-21 11:44:16 +0545

    Added more robust error handling

--- book_worm/spiders/bookworm.py
+++ book_worm/spiders/bookworm.py
@@ -9,37 +9,72 @@ class bookworm(scrapy.Spider):
     ]
 
     def parse(self, response):
-        book_list = response.css("li.col-xs-6.col-sm-4.col-md-3.col-lg-3") or response.xpath("//ol[@class='row']//li[contains(@class, 'col-xs-6')]")
-        for book in book_list:
-            url = book.css("a::attr(href)").get() or book.xpath("//div[@class='image_container']/a/@href").get()
-            yield response.follow(url, callback=self.bookData)
+        try:
+            book_list = response.css("li.col-xs-6.col-sm-4.col-md-3.col-lg-3") or response.xpath("//ol[@class='row']//li[contains(@class, 'col-xs-6')]")
+            for book in book_list:
+                url = book.css("a::attr(href)").get() or book.xpath("//div[@class='image_container']/a/@href").get()
+                yield response.follow(url, callback=self.bookData, errback=self.handle_error)
+        except Exception:
+            print(f"COULDN'T SCRAPE DATA DUE TO FAILURE IN LINK.")
         # handle pagination
         next_link = response.css("li.next a::attr(href)").get() or response.xpath("//li[@class='next']/a/@href").get()
-        yield response.follow(next_link, callback=self.parse)
+        yield response.follow(next_link, callback=self.parse, errback=self.handle_error)
 
     def bookData(self, response):
         """
             Collects the required information of each book from the given page and
             returns the data back to the parse method.
         """
-        name = response.css("div.col-sm-6.product_main h1::text").get() or response.xpath("//div[@class='product_main']/h1/text()").get()
-        price = response.css("div.col-sm-6.product_main p.price_color::text").get() or response.xpath("//div[@class='product_main']/p/text()").get()
+        name = response.css("div.col-sm-6.product_main h1::text").get() or response.xpath("//div[@class='product_main']/h1/text()").get() or "None"
+        price = response.css("div.col-sm-6.product_main p.price_color::text").get() or response.xpath("//div[@class='product_main']/p/text()").get() or "None"
         date_today = date.today().isoformat()
-        description = response.css("article.product_page p")[3].get()[3:-4] or response.xpath("//article[@class='product_page']/p/text()").get()
-        tax = response.css("table.table.table-striped tr td::text")[4].get() or response.xpath("//table/tr/td/text()")[4].get()
-        availability = response.css("table.table.table-striped tr td::text")[5].get() or response.xpath("//table/tr/td/text()")[5].get()
-        upc = response.css("table.table.table-striped tr td::text")[0].get() or response.xpath("//table/tr/td/text()")[0].get()
-        rating = response.css("div.col-sm-6.product_main p::attr(class)")[2].get().split(' ')[1] or response.xpath("//div[contains(@class, 'col-sm-6')]/p/@class").getall()[2].split()[1]
+        try:
+            description = response.xpath("//article[@class='product_page']/p/text()").get()
+            if not description:
+                description_more = response.css("article.product_page p::text").getall()[-1]
+                description = description_more[-1]
+        except Exception as e:
+            print("ERROR IN FINDING DESCRIPTION!")
+            description = "None"
+        try:
+            tax_more = response.css("table.table.table-striped tr td::text").getall() or response.xpath("//table/tr/td/text()").getall()
+            tax = tax_more[4]
+        except Exception as e:
+            print("ERROR IN FINDING TAX!")
+            tax = "None"
+        try:
+            availability_more = response.css("table.table.table-striped tr td::text").getall() or response.xpath("//table/tr/td/text()").getall()
+            availability = availability_more[5]
+        except Exception as e:
+            print("ERROR IN FINDING AVAILABILITY!")
+            availability = "None"
+        try:
+            upc_more = response.css("table.table.table-striped tr td::text").getall() or response.xpath("//table/tr/td/text()").getall()
+            upc = upc_more[0]
+        except Exception as e:
+            print("ERROR IN FINDING upc VALUE!")
+            upc = "None"
+        try:
+            rating_more = response.xpath("//div[contains(@class, 'col-sm-6')]/p/@class").getall()
+            rating = rating_more[2].split()[1]
+            if not rating:
+                rating_more = response.css("div.col-sm-6.product_main p::attr(class)")[2].get()
+                rating = rating_more.split(' ')[1]
+        except Exception as e:
+            print("ERROR IN FINDING RATING!")
+            rating = "None"
 
         yield {
-            "name": name if name else None,
-            "url": response.url if response.url else None,
+            "name": name,
+            "url": response.url or "None",
             "scrape_date": date_today,
-            "description": description if description else None,
-            "price": price if price else None,
-            "tax": tax if tax else None,
-            "availability": availability if availability else None,
-            "upc": upc if upc else None,
-            "rating": rating if rating else None
+            "description": description,
+            "price": price,
+            "tax": tax,
+            "availability": availability,
+            "upc": upc,
+            "rating": rating,
         }
 
+    def handle_error(self, failure):
+        self.logger.error(f"REQUEST FAILED: {failure.request.url}")
